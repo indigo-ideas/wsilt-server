b98b08e90b21f47c770aa43adc4b4fb33778eb00
Edmar Miyake <edmar.miyake@gmail.com>
2011-04-04 19:48:24 -0300

    project created

---
+++ wsilt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wsilt::Application.config.secret_token = '9444e82bb218071764bd4c0e2461b92b9bbc4e98f12c400d7f2c6456de1b3fb4b8852ccfc14357f9cd08b7cdcd1e5b98906cfa3c758fc6bceff81e38cc071afa'
